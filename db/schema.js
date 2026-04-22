8e8dba48c638c10a175464a3bd790d325159eda1
Muhammad <mh5458837@gmail.com>
2026-04-22 10:34:56 +0700

    Create Schema file

---
+++ db/schema.js
@@ -0,0 +1,20 @@
+import { pgTable, serial, text, boolean, timestamp, integer } from "drizzle-orm/pg-core";
+
+export const users = pgTable("absensi-users", {
+    id : serial("id").primaryKey(),
+    name: text("name").notNull(),
+    email: text("email").notNull().unique(),
+    password: text("password").notNull(),
+    role: text("role").notNull().default("user"),
+    is_active: boolean("is_active").default(true),
+});
+
+export const attendances = pgTable("attendances", {
+    id: serial("id").primaryKey(),
+    user_id: integer("user_id")
+        .notNull()
+        .references(() => users.id),
+    check_in: timestamp("check_in").defaultNow(),
+    check_out: timestamp("check_out"),
+    note: text("note"),
+});
