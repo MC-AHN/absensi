79fbd336260b3359c38fe19e1412fdbb5ac8b7cc
Muhammad <mh5458837@gmail.com>
2026-05-12 13:30:49 +0700

    deploy #2

--- api/index.js
+++ api/index.js
@@ -3,11 +3,14 @@ import { Hono } from "hono";
 import { auth } from "./auth.js";
 import { admin } from "./admin.js";
 import { user } from "./user.js";
+import { serveStatic } from "hono/serve-static";
 
 const app = new Hono();
 
 app.route("/auth", auth);
 app.route("/admin", admin);
 app.route("/user", user);
+
+app.use("/*", serveStatic({ root:"./public"  }))
 
 export default app;
